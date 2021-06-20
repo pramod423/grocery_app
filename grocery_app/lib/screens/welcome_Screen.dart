786c80de2f26e08c37b2725e3d07eaa561e0ff01
pramod423 <pramodudayanga14@gmail.com>
2021-06-20 10:58:49 +0530

    phone authontication

--- grocery_app/lib/screens/welcome_Screen.dart
+++ grocery_app/lib/screens/welcome_Screen.dart
@@ -2,6 +2,75 @@ import 'package:flutter/material.dart';
 import 'package:grocery_app/screens/onboard_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
+  bool _validPhoneNumber = false;
+  void showBottomSheet(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) =>
+            StatefulBuilder(builder: (context, StateSetter mystate) {
+              return Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'LOGIN',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'Enter your phone number to login',
+                          style: TextStyle(fontSize: 12, color: Colors.grey),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              prefixText: '+94',
+                              labelText: '10 digit Mobile Number'),
+                          autofocus: true,
+                          keyboardType: TextInputType.phone,
+                          maxLength: 10,
+                          onChanged: (value) {
+                            if (value.length == 10) {
+                              mystate(() {
+                                _validPhoneNumber = true;
+                              });
+                            } else {
+                              mystate(() {
+                                _validPhoneNumber = false;
+                              });
+                            }
+                          },
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: AbsorbPointer(
+                                absorbing: _validPhoneNumber ? false : true,
+                                child: FlatButton(
+                                    color: _validPhoneNumber
+                                        ? Theme.of(context).primaryColor
+                                        : Colors.grey,
+                                    onPressed: () {},
+                                    child: Text(
+                                      _validPhoneNumber
+                                          ? 'CONTINUE'
+                                          : 'ENTER PHONE NUMBER',
+                                      style: TextStyle(color: Colors.white),
+                                    )),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +103,9 @@ class WelcomeScreen extends StatelessWidget {
                   height: 20,
                 ),
                 FlatButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      showBottomSheet(context);
+                    },
                     child: RichText(
                         text: TextSpan(
                             text: 'Already a Customer ?',
